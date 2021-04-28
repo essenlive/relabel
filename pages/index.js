9170d39b3bea308b7d8c42509fdc7d4fb186a87d
Quentin Perchais <perchais.quentin@gmail.com>
2021-04-28 14:22:32 +0200

    🎨Minor forms layout changes

--- pages/index.js
+++ pages/index.js
@@ -34,73 +34,115 @@ export default function Home() {
             handleBlur,
             handleSubmit,
             isSubmitting,
-          }) =>{
-            return(
-            <>
-              <div className={styles.form}>
-                <form onSubmit={handleSubmit}>
-                  <input
-                    name="name"
-                    placeholder="Nom de la structure"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.name}
-                  />
-                  <input
-                    name="status"
-                    placeholder="Type de structure"
-                    onChange={handleChange}
-                    onBlur={handleBlur}
-                    value={values.status}
-                  />
-                  <input
-                    type="number"
-                    name="partnersCount"
-                    placeholder="Nombre de partenaires"
-                    value={values.partnersCount}
-                    onBlur={handleBlur}
-                    onChange={handleChange} />
-                  <input
-                    type="number"
-                    name="materials"
-                    placeholder="Pourcentage de materiaux"
-                    value={values.materials}
-                    onBlur={handleBlur}
-                    onChange={handleChange} />
-                  <input
-                    type="number"
-                    name="production"
-                    placeholder="Pourcentage de production"
-                    value={values.production}
-                    onBlur={handleBlur}
-                    onChange={handleChange} />
-                  <input
-                    type="number"
-                    name="gestion"
-                    placeholder="Pourcentage de gestion"
-                    value={values.gestion}
-                    onBlur={handleBlur}
-                    onChange={handleChange} />
-                  {/* <button type="submit" disabled={isSubmitting}>
+          }) => {
+            return (
+              <>
+                <div className={styles.form}>
+                  <form onSubmit={handleSubmit}>
+                    <div className={styles.input}>
+                      <span>
+                        Nom : 
+                      </span>
+                      <input
+                        name="name"
+                        placeholder="Nom de la structure"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.name}
+                      />
+                    </div>
+                    <div className={styles.input}>
+                      <span>
+                        Type :
+                      </span>
+                      <select
+                        dir="rtl"
+                        name="status"
+                        placeholder="Type de structure"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.status}
+                      >
+                        <option value="SARL">SARL</option>
+                        <option value="SAS">SAS</option>
+                        <option value="SA">SA</option>
+                        <option value="Association">Association</option>
+                        <option value="SCOP">SCOP</option>
+                        <option value="SCIC">SCIC</option>
+                      </select>        
+                    </div>
+                    <div className={styles.input}>
+                      <span>
+                        Nombre de partenaires :
+                      </span>
+                      <input
+                        type="number"
+                        name="partnersCount"
+                        placeholder="Nombre de partenaires"
+                        value={values.partnersCount}
+                        onBlur={handleBlur}
+                        onChange={handleChange} />
+                      <label>partenaire.s</label>
+                    </div>
+                    <div className={styles.input}>
+                      <span>
+                        Pourcentage de matériaux :
+                      </span>
+                      <input
+                        type="number"
+                        name="materials"
+                        placeholder="Pourcentage de materiaux"
+                        value={values.materials}
+                        onBlur={handleBlur}
+                        onChange={handleChange} />
+                        <label>%</label>
+                    </div>
+                    <div className={styles.input}>
+                      <span>
+                        Pourcentage de production :
+                      </span>
+                      <input
+                        type="number"
+                        name="production"
+                        placeholder="Pourcentage de production"
+                        value={values.production}
+                        onBlur={handleBlur}
+                        onChange={handleChange} />
+                      <label>%</label>
+                    </div>
+                    <div className={styles.input}>
+                      <span>
+                        Pourcentage de gestion :
+                      </span>
+                      <input
+                        type="number"
+                        name="gestion"
+                        placeholder="Pourcentage de gestion"
+                        value={values.gestion}
+                        onBlur={handleBlur}
+                        onChange={handleChange} />
+                      <label>%</label>
+                    </div>
+                    {/* <button type="submit" disabled={isSubmitting}>
                     Submit
            </button> */}
-                </form>
-              </div>
+                  </form>
+                </div>
 
 
-              <Label
-                title={values.name}
-                status={values.status}
-                date={'15.11.90'}
-                size={'medium'}
-                data={{
-                  partners: Number(values.partnersCount),
-                  production: Number(values.production),
-                  gestion: Number(values.gestion),
-                  materials: Number(values.materials),
-                }} />
-            </>
-          )
+                <Label
+                  title={values.name}
+                  status={values.status}
+                  date={'2021'}
+                  size={'medium'}
+                  data={{
+                    partners: Number(values.partnersCount),
+                    production: Number(values.production / 100),
+                    gestion: Number(values.gestion / 100),
+                    materials: Number(values.materials / 100),
+                  }} />
+              </>
+            )
           }}
         </Formik>
 
