dea1ce37364941dd0f5ee19c661aed8533552720
essenlive <perchais.quentin@gmail.com>
2021-09-13 16:46:10 +0200

    🎨 Added responsive menu

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,47 +1,57 @@
 import Link from 'next/link'
+import { useState } from 'react';
 import { useRouter } from 'next/router'
+import { FiMenu, FiX } from 'react-icons/fi'
+import classNames from "classnames"
 import styles from "@styles/components/Navigation.module.css";
 
 export default function Navigation(props) {
   const router = useRouter()
+  const [menuOpen, setMenuOpen] = useState(false);
 
     return (
-      <nav  className={styles.nav}>
-        <Link href="/">
-          <div className={styles.logo}>
-            <a>
-              <img src="/assets/logo.png" alt="logo relabel" />
-            </a>
-          </div>
-        </Link>
-        <div className={styles.menu}>
-          <Link href="/structures">
-            <div className={router.route === '/structures' ? styles.linkActive : styles.links}>
-              <a>Carte</a>
-            </div>
-          </Link>
-          <Link href="/communities">
-            <div className={router.route === '/communities' ? styles.linkActive : styles.links}>
-              <a>Communautés</a>
-            </div>
-          </Link>
-          <Link href="/projects">
-            <div className={router.route === '/projects' ? styles.linkActive : styles.links}>
-              <a>Productions</a>
-            </div>
-          </Link>
-          {/* <Link href="/practices">
-            <div className={router.route === '/practices' ? styles.linkActive : styles.links}>
-              <a>Pratiques</a>
-            </div>
-          </Link> */}
-          {/* <Link href="/contact">
-            <div className={router.route === '/contact' ? styles.linkActive : styles.linksEmphasis}>
-              <a>Nous contacter</a>
-            </div>
-          </Link> */}
-        </div>
+        
+        <nav className={classNames(styles.nav, { [`${styles.navOpen}`]: menuOpen})}>
+         <Link onClick={() => { setMenuOpen(false) }} href="/">
+           <div className={styles.logo}>
+             <a>
+               <img src="/assets/logo.png" alt="logo relabel" />
+             </a>
+           </div>
+         </Link>
+        <div className={classNames(styles.menu, { [`${styles.open}`]: menuOpen })}>
+           <Link onClick={() => { setMenuOpen(false) }} href="/structures">
+            <div className={classNames(styles.links, { [`${styles.linkActive}`]: router.route === '/structures' }, { [`${styles.open}`]: menuOpen })}>
+               <a>Carte</a>
+             </div>
+           </Link>
+           <Link onClick={() => { setMenuOpen(false) }} href="/communities">
+             <div className={classNames(styles.links,{ [`${styles.linkActive}`]: router.route === '/communities' }, { [`${styles.open}`]: menuOpen })}>
+               <a>Communautés</a>
+             </div>
+           </Link>
+           <Link onClick={() => { setMenuOpen(false) }} href="/projects">
+            <div className={classNames(styles.links, { [`${styles.linkActive}`]: router.route === '/projects' }, { [`${styles.open}`]: menuOpen })}>
+               <a>Productions</a>
+             </div>
+           </Link>
+           {/* <Link onClick={() => { setMenuOpen(false) }} href="/practices">
+            <div className={classNames(styles.links, { [`${styles.linkActive}`]: router.route === '/practices' }, { [`${styles.open}`]: menuOpen })}>
+               <a>Pratiques</a>
+             </div>
+           </Link> */}
+           <Link onClick={() => { setMenuOpen(false) }} href="/contact">
+            <div className={classNames(styles.linksEmphasis, { [`${styles.linkActive}`]: router.route === '/contact' }, { [`${styles.open}`]: menuOpen })}>
+               <a>Nous contacter</a>
+             </div>
+           </Link>
 
-      </nav>
+           <div className={styles.MobileToggle} onClick={() => { setMenuOpen(!menuOpen) }}>
+            {menuOpen && (<span><FiX /></span>)}
+            {!menuOpen && (<span>MENU <FiMenu /></span>)}
+           </div>
+         </div>
+
+       </nav>
     );
 }
