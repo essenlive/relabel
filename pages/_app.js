c77babd19e5be3d92df2926abf1627e92f07b9fc
essenlive <perchais.quentin@gmail.com>
2024-02-19 09:47:44 +0100

    ⚡ Added images

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import "@styles/globals/globals.css";
 import "@styles/globals/theme.css";
 import "@styles/globals/map.css";
-import 'mapbox-gl/dist/mapbox-gl.css';
+// import 'mapbox-gl/dist/mapbox-gl.css';
 import React from 'react';
 
 
